1132c8d1c1b9cb6de56b76258c8aa5895d810902
Joy Parikh <52308264+joyparikh@users.noreply.github.com>
2021-12-10 00:05:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='rubix',
+setup(name='gym_Rubiks_Cube',
       version='0.0.1',
       install_requires=['gym', 'termcolor'],
       url="https://github.com/joyparikh/ELEN6885-Project",
