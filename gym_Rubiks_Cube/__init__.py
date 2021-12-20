d745760814edde8fb08fa265d3d59317125bcaa3
Joy Parikh <52308264+joyparikh@users.noreply.github.com>
2021-12-20 08:32:07 +0530

    add new env

--- gym_Rubiks_Cube/__init__.py
+++ gym_Rubiks_Cube/__init__.py
@@ -6,6 +6,18 @@ register(
 )
 
 register(
+    id='RubiksCube-v1',
+    entry_point='gym_Rubiks_Cube.envs:RubiksCubeEnv',
+    kwargs={'optimize' = True}
+)
+
+register(
+    id='RubiksCube-v2',
+    entry_point='gym_Rubiks_Cube.envs:RubiksCubeEnv',
+    kwargs={'optimize' = True, 'ultragoal' = True}
+)
+
+register(
     id='RubiksCube2x2-v0',
     entry_point='gym_Rubiks_Cube.envs:RubiksCubeEnv',
     kwargs={'orderNum' : 2},
